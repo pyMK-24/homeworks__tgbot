8bffdad49d3696fa81d15924e4c8d289e5a4bdb0
pyMK-24 <karimmohammad1406@gmail.com>
2025-01-11 18:03:07 +0600

    sec_1 com

--- handlers/menu.py
+++ handlers/menu.py
@@ -6,12 +6,12 @@ menu_router = Router()
 @menu_router.message(Command("menu"))
 async def menu_handler(message: types.Message):
     await message.answer("""Вот наше меню:
-Мясная с аджикой 30cm, /pic_myasnay 
-Баварская 30cm, /pic_bavarskay
-Сырная 30cm, /pic_sirnay
-Ветчина и сыр 30 cm, /pic_vetchina_sir
-Двойной цыпленок 30cm, /pic_dvoinoi_ciplenok
-Маргарита 30cm, /pic_margarita
-Песто 30cm, /pic_pesto
-Карбонара 30cm, /pic_karbonara
-Аррива! 30cm, /pic_arriva """)
+Мясная с аджикой 30cm
+Баварская 30cm
+Сырная 30cm
+Ветчина и сыр 30 cm
+Двойной цыпленок 30cm
+Маргарита 30cm
+Песто 30cm
+Карбонара 30cm
+Аррива! 30cm""")
